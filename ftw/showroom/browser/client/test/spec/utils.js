3b368332ed04bd7e2fc0756ac6846f18b02a1dc4
Elio Schmutz <e.schmutz@4teamwork.ch>
2016-05-04 16:01:38 +0200

    Fix typos

--- ftw/showroom/browser/client/test/spec/utils.js
+++ ftw/showroom/browser/client/test/spec/utils.js
@@ -2,16 +2,16 @@ import { noop, uuid, isHTMLElement } from "utils"
 
 describe("Utils", () => {
 
-  it("itHTMLElement should recongnize DOM element", () => {
+  it("itHTMLElement should recognize DOM element", () => {
     var htmlElement = document.createElement("a");
     var plainObject = {};
     var string = "string";
     var number = 2;
 
-    assert.isTrue(isHTMLElement(htmlElement), "htmlElement sould be recongnized as an htmlElement");
-    assert.isFalse(isHTMLElement(plainObject), "plainObject sould not be recongnized as an htmlElement");
-    assert.isFalse(isHTMLElement(string), "string sould not be recongnized as an htmlElement");
-    assert.isFalse(isHTMLElement(number), "number sould not be recongnized as an htmlElement");
+    assert.isTrue(isHTMLElement(htmlElement), "htmlElement should be recognized as an htmlElement");
+    assert.isFalse(isHTMLElement(plainObject), "plainObject should not be recognized as an htmlElement");
+    assert.isFalse(isHTMLElement(string), "string should not be recognized as an htmlElement");
+    assert.isFalse(isHTMLElement(number), "number should not be recognized as an htmlElement");
   });
 
 });
