6e205be6af0dcb9dc83ca30b261badf9f1f89016
Jonas Baumann <jone@jone.ch>
2019-11-12 08:28:23 +0100

    Fix tests.
    Use unittest instead of unittest2 (dependency was not declared).

--- ftw/showroom/tests/__init__.py
+++ ftw/showroom/tests/__init__.py
@@ -1,7 +1,7 @@
 from ftw.showroom.testing import FTW_SHOWROOM_FUNCTIONAL
 from plone.app.testing import setRoles
 from plone.app.testing import TEST_USER_ID
-from unittest2 import TestCase
+from unittest import TestCase
 import transaction
 
 
